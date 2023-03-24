554a47083f8ed6f331fb4010cb968dac6b2fbb83
svcase <59538644+svcase@users.noreply.github.com>
2023-03-24 10:29:16 -0500

    event handler

--- index.js
+++ index.js
@@ -8,10 +8,16 @@ secondCard.addEventListener('mouseover', ticText);
 thirdCard.addEventListener('mouseover', widjiText);
 fourthCard.addEventListener('mouseover', rpsText);
 
+firstCard.addEventListener('touchstart', lizaText);
+secondCard.addEventListener('touchstart', ticText);
+thirdCard.addEventListener('touchstart', widjiText);
+fourthCard.addEventListener('touchstart', rpsText);
+
 function lizaText() {
     let originalText = firstCard.firstElementChild.innerText;
     let text = 'HTML, CSS, JavaScript, basic React';
     firstCard.firstElementChild.innerText = text;
+    firstCard.addEventListener('touchend', ()=>{firstCard.firstElementChild.innerText = originalText});
     firstCard.addEventListener('mouseout', ()=>{firstCard.firstElementChild.innerText = originalText});
 }
 
@@ -19,6 +25,7 @@ function ticText() {
     let originalText = secondCard.firstElementChild.innerText;
     let text = 'Vanilla JavaScript';
     secondCard.firstElementChild.innerText = text;
+    secondCard.addEventListener('touchend', ()=>{secondCard.firstElementChild.innerText = originalText});
     secondCard.addEventListener('mouseout', ()=>{secondCard.firstElementChild.innerText = originalText});
 }
 
@@ -26,6 +33,7 @@ function widjiText() {
     let originalText = thirdCard.firstElementChild.innerText;
     let text = 'HTML, CSS, JavaScript, basic React';
     thirdCard.firstElementChild.innerText = text;
+    thirdCard.addEventListener('touchend', ()=>{thirdCard.firstElementChild.innerText = originalText});
     thirdCard.addEventListener('mouseout', ()=>{thirdCard.firstElementChild.innerText = originalText});
 }
 
@@ -33,5 +41,6 @@ function rpsText() {
     let originalText = fourthCard.firstElementChild.innerText;
     let text = 'Vanilla Javascript';
     fourthCard.firstElementChild.innerText = text;
+    fourthCard.addEventListener('touchend', ()=>{fourthCard.firstElementChild.innerText = originalText});
     fourthCard.addEventListener('mouseout', ()=>{fourthCard.firstElementChild.innerText = originalText});
 }
